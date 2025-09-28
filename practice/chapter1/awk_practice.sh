5b8a1bbc710a0542e824e9ed2feaf9a3180b307a
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2025-09-28 12:35:10 +0900

    feat: 設定ファイルからの情報抽出

--- practice/chapter1/awk_practice.sh
+++ practice/chapter1/awk_practice.sh
@@ -14,3 +14,6 @@ seq 5 | awk '$1%2==0{printf("%s 偶数\n", $1)}$1%2{printf("%s 奇数\n", $1)}'
 
 # 複数の条件と処理と終了後の処理
 seq 5 | awk 'BEGIN{a=0}$1%2==0{printf("%s 偶数\n", $1)}$1%2{printf("%s 奇数\n", $1)}{a+=$1}END{print "合計", a}'
+
+# 設定ファイルからの情報抽出
+cat ../../qdata/5/ntp.conf | awk '$1=="pool"' | awk '{print $2}'
